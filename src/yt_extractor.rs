cfe9674f8887f00d8bcd1b56c2299b3ed9b6337b
Roberto Orellana <rorepoid@gmail.com>
2021-12-01 22:59:31 -0500

    ♻ Sort functions Separate public and private functions

--- src/yt_extractor.rs
+++ src/yt_extractor.rs
@@ -14,32 +14,6 @@ impl YouTubeExtractor {
         Self { url }
     }
 
-    fn extract(&self) -> Result<String, ExtractorError> {
-        let mut text: String = String::new();
-        println!("Extracting video info...");
-        reqwest::blocking::get(self.url)?.read_to_string(&mut text)?;
-
-        Ok(text)
-    }
-
-    fn find_regex(&self, text: &str) -> Option<String> {
-        Regex::new(r"(ytInitialPlayerResponse\s*=\s*)(\{.+?});")
-            .ok()?
-            .captures(text)?
-            .get(2)
-            .map(|value| value.as_str().to_owned())
-    }
-
-    pub fn player_response(&self) -> Result<PlayerResponse, ExtractorError> {
-        let extraction = self.extract()?;
-
-        let player_response = self
-            .find_regex(&extraction)
-            .ok_or(ExtractorError::PlayerResponseNotFound)?;
-
-        PlayerResponse::new(&player_response)
-    }
-
     pub fn download(self) -> Result<(), ExtractorError> {
         let url = self
             .player_response()?
@@ -59,5 +33,31 @@ impl YouTubeExtractor {
         file.write_all(&data)?;
 
         Ok(())
+    }
+
+    fn extract(&self) -> Result<String, ExtractorError> {
+        let mut text: String = String::new();
+        println!("Extracting video info...");
+        reqwest::blocking::get(self.url)?.read_to_string(&mut text)?;
+
+        Ok(text)
+    }
+
+    fn find_regex(&self, text: &str) -> Option<String> {
+        Regex::new(r"(ytInitialPlayerResponse\s*=\s*)(\{.+?});")
+            .ok()?
+            .captures(text)?
+            .get(2)
+            .map(|value| value.as_str().to_owned())
+    }
+
+    fn player_response(&self) -> Result<PlayerResponse, ExtractorError> {
+        let extraction = self.extract()?;
+
+        let player_response = self
+            .find_regex(&extraction)
+            .ok_or(ExtractorError::PlayerResponseNotFound)?;
+
+        PlayerResponse::new(&player_response)
     }
 }
